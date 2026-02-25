ad2ba4f7886156f7236faaa9c40a80df02fbc896
ThitipongPP <thitipongpp@MacBook-Air-khxng-ThitipongPP.local>
2026-02-26 00:12:59 +0700

    Add login catch in auth controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -38,39 +38,47 @@ exports.register = async (req, res, next) => {
 // @route   POST /auth/login
 // @access  Public
 exports.login = async (req, res, next) => {
-    // Get body request
-    const {email, password} = req.body;
+    try {
+        // Get body request
+        const {email, password} = req.body;
 
-    // Validate email & password
-    if (!email || !password) return res.status(400).json({
-        success: false,
-        message: "Please provide an email and password"
-    });
+        // Validate email & password
+        if (!email || !password) return res.status(400).json({
+            success: false,
+            message: "Please provide an email and password"
+        });
 
-    // Find user in database
-    const user = await User.findOne({email}).select('+password');
+        // Find user in database
+        const user = await User.findOne({email}).select('+password');
 
-    // Don't find user in database
-    if (!user) return res.status(400).json({
-        success: false,
-        message: "Invalid credentials"
-    });
+        // Don't find user in database
+        if (!user) return res.status(400).json({
+            success: false,
+            message: "Invalid credentials"
+        });
 
-    // Check matched password
-    const isMatch = await user.matchPassword(password);
+        // Check matched password
+        const isMatch = await user.matchPassword(password);
 
-    // Password don't match
-    if (!isMatch) return res.status(401).json({
-        success: false,
-        message: "Invalid password"
-    });
+        // Password don't match
+        if (!isMatch) return res.status(401).json({
+            success: false,
+            message: "Invalid password"
+        });
 
-    // Create token for JWT
-    const token = user.getSignedJwtToken();
+        // Create token for JWT
+        const token = user.getSignedJwtToken();
 
-    res.status(200).json({
-        success: true,
-        data: user,
-        token
-    });
+        res.status(200).json({
+            success: true,
+            data: user,
+            token
+        });
+    }catch (err) {
+        res.status(400).json({
+            success: false,
+            message: err.message
+        });
+        console.error(err.message);
+    }
 };
