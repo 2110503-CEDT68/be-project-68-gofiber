97f89447594d05e030b8cf382933ec94cbf65f24
ThitipongPP <thitipongpp@MacBook-Air-khxng-ThitipongPP.local>
2026-02-26 00:41:59 +0700

    Add get logged in user route in auth route

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,17 @@
 const express = require('express');
 
 // Controller file
-const {register, login} = require('../controllers/auth');
+const {register, login, me} = require('../controllers/auth');
 
 // Router
 const router = express.Router();
 
+// Auth Middleware
+const {protect} = require('../middleware/auth');
+
 // Path and method
 router.post('/register', register);
 router.post('/login', login);
+router.get('/me', protect, me);
 
 module.exports = router;
