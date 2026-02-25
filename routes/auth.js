cb0edf77e21a7f112f6586dbe5adaeeeea49fcf7
ThitipongPP <thitipongpp@MacBook-Air-khxng-ThitipongPP.local>
2026-02-26 00:13:24 +0700

    Add login router to auth route

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,13 @@
 const express = require('express');
 
 // Controller file
-const {register} = require('../controllers/auth');
+const {register, login} = require('../controllers/auth');
 
 // Router
 const router = express.Router();
 
 // Path and method
 router.post('/register', register);
+router.post('/login', login);
 
 module.exports = router;
